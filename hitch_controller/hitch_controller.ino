f78c4db072296d3f4f8753def9cc85dffa5709f5
Jeremy <jeremy.noonan@torqit.ca>
2023-01-06 20:18:03 -0400

    basic print

--- hitch_controller/hitch_controller.ino
+++ hitch_controller/hitch_controller.ino
@@ -1,13 +1,48 @@
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+//LCD1602
+//You should now see your LCD1602 display the flowing characters "SUNFOUNDER" and "hello, world"
+//Email:info@primerobotics.in
+//Website:www.primerobotics.in
+//2015.5.7 
+#include <LiquidCrystal.h>// include the library code
+/**********************************************************/
+char array1[]="Hi Shawn";  //the string to print on the LCD
+char array2[]="You look nice 8====D ";  //the string to print on the LCD
+int tim = 250;  //the value of delay time
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+/*********************************************************/
 
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(3000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(3000);                      // wait for a second
+int recordedHitchHeight = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(16, 2);  // set up the LCD's number of columns and rows: 
+
+  lcd.setCursor(0,0);  // set the cursor to column 15, line 0
+  lcd.print("Hitch Set: 50%");
+  lcd.setCursor(0,1);
+  lcd.print("Hitch Cur: 24%");
 }
+/*********************************************************/
+void loop() 
+{
+
+
+  float currentHitchHeightFloat  = 100 - (analogRead(A5) / 1023.0 * 100);
+  int currentHitchHeight = (int)currentHitchHeightFloat;
+  if(currentHitchHeight != recordedHitchHeight){
+    recordedHitchHeight = currentHitchHeight;   
+
+    lcd.clear();
+    lcd.setCursor(0,0);  // set the cursor to column 15, line 0
+    lcd.print("Hitch Set: ");
+    lcd.print(currentHitchHeight);
+    lcd.print("%");
+    lcd.setCursor(0,1);
+    lcd.print("Hitch Cur: 24%");
+  }
+
+  delay(50);
+}
+/************************************************************/
