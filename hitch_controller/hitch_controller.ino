9eaf078e7cd9f2d56301a2a4afa9e1af34375bc8
Jeremy <jeremy.noonan@torqit.ca>
2023-03-11 10:38:09 -0400

    calibration

--- hitch_controller/hitch_controller.ino
+++ hitch_controller/hitch_controller.ino
@@ -5,14 +5,16 @@
 //2015.5.7 
 #include <LiquidCrystal.h>// include the library code
 /**********************************************************/
-char array1[]="Hi Shawn";  //the string to print on the LCD
-char array2[]="You look nice 8====D ";  //the string to print on the LCD
-int tim = 250;  //the value of delay time
+
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
 /*********************************************************/
 
-int recordedHitchHeight = 0;
+int recordedSetHitchHeight = 0;
+int recordedSensorHeight = 0;
+
+int averagedReadings[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int readingCount = 0;
 
 void setup()
 {
@@ -23,26 +25,45 @@ void setup()
   lcd.print("Hitch Set: 50%");
   lcd.setCursor(0,1);
   lcd.print("Hitch Cur: 24%");
+
 }
 /*********************************************************/
 void loop() 
 {
 
 
-  float currentHitchHeightFloat  = 100 - (analogRead(A5) / 1023.0 * 100);
-  int currentHitchHeight = (int)currentHitchHeightFloat;
-  if(currentHitchHeight != recordedHitchHeight){
-    recordedHitchHeight = currentHitchHeight;   
+  float currenSetHitchHeightFloat  = 100 - (analogRead(A0) / 1023.0 * 100);
+  int currenSetHitchHeight = (int)currenSetHitchHeightFloat;
+
+  averagedReadings[readingCount] = analogRead(A5);
+  readingCount++;
+  if(readingCount == 80){
+    readingCount = 0;
+  }
+
+  int pAverage = 0;
+
+  for(int i = 0; i < 80; i++){
+    pAverage += averagedReadings[i];
+  }
+
+  pAverage = pAverage / 80;
+
+  Serial.print("Prenus ");
+  Serial.println(pAverage);
+  
+  if(abs(currenSetHitchHeight-recordedSetHitchHeight) >2){
+    recordedSetHitchHeight = currenSetHitchHeight;   
 
     lcd.clear();
     lcd.setCursor(0,0);  // set the cursor to column 15, line 0
     lcd.print("Hitch Set: ");
-    lcd.print(currentHitchHeight);
+    lcd.print(currenSetHitchHeight);
     lcd.print("%");
     lcd.setCursor(0,1);
     lcd.print("Hitch Cur: 24%");
   }
 
-  delay(50);
+  delay(2);
 }
 /************************************************************/
